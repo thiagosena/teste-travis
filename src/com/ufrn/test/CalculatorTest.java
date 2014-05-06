c4aef1c4ecf0b82af70cd34b062a74db7950bc85
Thiago César <thiagoalves@lcc.ufrn.br>
2014-05-06 01:57:23 -0300

    Adicionando metodo multiply e criando os testes

---
+++ src/com/ufrn/test/CalculatorTest.java
@@ -0,0 +1,33 @@
+package com.ufrn.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.ufrn.first.Calculator;
+
+public class CalculatorTest {
+
+	@BeforeClass
+	public static void testSetup() {
+	}
+
+	@AfterClass
+	public static void testCleanup() {
+		// Teardown for data used by the unit tests
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testExceptionIsThrown() {
+		Calculator test = new Calculator();
+		test.multiply(1000, 5);
+	}
+
+	@Test
+	public void testMultiply() {
+		Calculator test = new Calculator();
+		assertEquals("10 x 5 must be 50", 50, test.multiply(10, 5));
+	}
+}
